73a91db190a3030828bcaeacebd33873e685a687
ssyed04 <SULAYMANSYED11@GMAIL.COM>
2021-03-28 00:38:58 -0400

    Added more stuff

--- WinHacks.py/test1.py
+++ WinHacks.py/test1.py
@@ -1,4 +1,5 @@
 from pygame import *
+from pygame import movie
 
 width, height = 800, 600
 screen = display.set_mode((width, height))
@@ -11,13 +12,16 @@ yellow = (255, 255, 0)
 white = (255, 255, 255)
 bg = (50, 62, 79)
 
+#open stats---------------------------------------------------------------------
+open("stats.txt", "r")
+
 ##RECTANGLES--------------------------------------------------------------------
-p1Select1 = Rect(50, 125, 175, 140)
-p1Select2 = Rect(50, 350, 175, 140)
-p1Select3 = Rect(300, 125, 175, 140)
-p1Select4 = Rect(300, 350, 175, 140)
-p1Select5 = Rect(550, 125, 175, 140)
-p1Select6 = Rect(550, 350, 175, 140)
+LeBron_JamesPick = Rect(50, 125, 175, 140)
+Kawhi_LeonardPick = Rect(50, 350, 175, 140)
+Stephen_CurryPick = Rect(300, 125, 175, 140)
+Nikola_JokicPick = Rect(300, 350, 175, 140)
+GiannisPick = Rect(550, 125, 175, 140)
+Kevin_DurantPick = Rect(550, 350, 175, 140)
 
 p2Select1 = Rect(50, 125, 175, 140)
 p2Select2 = Rect(50, 350, 175, 140)
@@ -49,15 +53,15 @@ p5Select6 = Rect(550, 350, 175, 140)
 
 
 ## loading pics for pick 1
-picNames=["Pics/lebron.png", "Pics/kawhi.png",
-          "Pics/curry.png", "Pics/jokic.png",
-          "Pics/giannis.png", "Pics/durant.png",
-          "Pics/luka.png", "Pics/westbrook.png",
-          "Pics/tatum.png", "Pics/zach.png",
-          "Pics/harden.png", "Pics/zion.png",
-          "Pics/beal.png", "Pics/trae.png",
-          "Pics/embiid.png", "Pics/ben.png",
-          "Pics/randle.png", "Pics/lowry.png",]
+picNames=["WinHacks.py/Pics/lebron.png", "WinHacks.py/Pics/kawhi.png",
+          "WinHacks.py/Pics/curry.png", "WinHacks.py/Pics/jokic.png",
+          "WinHacks.py/Pics/giannis.png", "WinHacks.py/Pics/durant.png",
+          "WinHacks.py/Pics/luka.png", "WinHacks.py/Pics/westbrook.png",
+          "WinHacks.py/Pics/tatum.png", "WinHacks.py/Pics/zach.png",
+          "WinHacks.py/Pics/harden.png", "WinHacks.py/Pics/zion.png",
+          "WinHacks.py/Pics/beal.png", "WinHacks.py/Pics/trae.png",
+          "WinHacks.py/Pics/embiid.png", "WinHacks.py/Pics/ben.png",
+          "WinHacks.py/Pics/randle.png", "WinHacks.py/Pics/lowry.png"]
 
 myPicList = []
 for name in picNames:
@@ -65,8 +69,8 @@ for name in picNames:
     picc = transform.smoothscale(pic, (175, 140))
     myPicList.append(picc)
 
-rectList1 = [p1Select1, p1Select2, p1Select3,
-            p1Select4, p1Select5, p1Select6]
+rectList1 = [LeBron_JamesPick, Kawhi_LeonardPick, Stephen_CurryPick,
+            Nikola_JokicPick, GiannisPick, Kevin_DurantPick]
 
 rectList2 = [p2Select1, p2Select2, p2Select3,
             p2Select4, p2Select5, p2Select6]
@@ -81,27 +85,31 @@ rectList5 = [p5Select1, p5Select2, p5Select3,
             p5Select4, p5Select5, p5Select6]
 
 screen.fill(bg)
-screen.blit(myPicList[0], p1Select1)
-screen.blit(myPicList[1], p1Select2)
-screen.blit(myPicList[2], p1Select3)
-screen.blit(myPicList[3], p1Select4)
-screen.blit(myPicList[4], p1Select5)
-screen.blit(myPicList[5], p1Select6)
-
+P1Team = []
+P2Team = []
 count = 0
-
-running = True
-
-while running:
-
-    mx, my = mouse.get_pos()
-    mb = mouse.get_pressed()
-
-    for evt in event.get():
-        if evt.type == QUIT:
-            running = False
-
+menu = 'p1pick1'
+def p1pick1():
     for rect in rectList1:
+        draw.rect(screen, white, rect, 1)
+        #draw.rect(screen,black,rect,1)
+        if rect.collidepoint(mx,my):
+            draw.rect(screen, gray, rect, 3)         #highlights the tool rects if the mouse hovers over
+        #blitting the text box in accordance with the tool
+            # if mb[0] == 1:
+            #     return p1pick2()
+        else:
+            draw.rect(screen, white, rect, 3)
+
+    screen.blit(myPicList[0], LeBron_JamesPick)
+    screen.blit(myPicList[1], Kawhi_LeonardPick)
+    screen.blit(myPicList[2], Stephen_CurryPick)
+    screen.blit(myPicList[3], Nikola_JokicPick)
+    screen.blit(myPicList[4], GiannisPick)
+    screen.blit(myPicList[5], Kevin_DurantPick)
+
+def p1pick2():
+    for rect in rectList2:
         draw.rect(screen, white, rect, 1)
         #draw.rect(screen,black,rect,1)
         if rect.collidepoint(mx,my):
@@ -110,26 +118,80 @@ while running:
         else:
             draw.rect(screen, white, rect, 3)
 
-    for rect in rectList1:
-        if rect.collidepoint(mx, my):
-            screen.fill(bg)
-            screen.blit(myPicList[6], p2Select1)
-            screen.blit(myPicList[7], p2Select2)
-            screen.blit(myPicList[8], p2Select3)
-            screen.blit(myPicList[9], p2Select4)
-            screen.blit(myPicList[10], p2Select5)
-            screen.blit(myPicList[11], p2Select6)
+    screen.blit(myPicList[6], LeBron_JamesPick)
+    screen.blit(myPicList[7], Kawhi_LeonardPick)
+    screen.blit(myPicList[8], Stephen_CurryPick)
+    screen.blit(myPicList[9], Nikola_JokicPick)
+    screen.blit(myPicList[10], GiannisPick)
+    screen.blit(myPicList[11], Kevin_DurantPick)
 
-    for rect in rectList2:
-        if rect.collidepoint(mx, my) and mb[0] == 1:
-            screen.fill(bg)
-            screen.blit(myPicList[12], p3Select1)
-            screen.blit(myPicList[13], p3Select2)
-            screen.blit(myPicList[14], p3Select3)
-            screen.blit(myPicList[15], p3Select4)
-            screen.blit(myPicList[16], p3Select5)
-            screen.blit(myPicList[17], p3Select6)
-    print(count)
+    # for rect in rectList1:
+    #     if rect.collidepoint(mx, my):
+    #         screen.fill(bg)
+    #         screen.blit(myPicList[6], p2Select1)
+    #         screen.blit(myPicList[7], p2Select2)
+    #         screen.blit(myPicList[8], p2Select3)
+    #         screen.blit(myPicList[9], p2Select4)
+    #         screen.blit(myPicList[10], p2Select5)
+    #         screen.blit(myPicList[11], p2Select6)
+
+
+running = True
+
+while running:
+    print(menu)
+    screen.fill(bg)
+    mx, my = mouse.get_pos()
+    mb = mouse.get_pressed()
+
+    for evt in event.get():
+        for rect in rectList1:
+            if rect.collidepoint(mx, my) and evt.type == MOUSEBUTTONUP:
+                if menu == 'p1pick1':
+                    menu = 'p1pick2'
+
+        if evt.type == QUIT:
+            running = False
+    if menu == 'intro':
+        intro()
+    if menu == 'instructions':
+        instructions()
+    if menu == 'p1pick1':
+        p1pick1()
+    if menu == 'p1pick2':
+        p1pick2()
+    if menu == 'p1pick3':
+        p1pick3()
+    if menu == 'p1pick4':
+        p1pick4()
+    if menu == 'p1pick5':
+        p1pick5()
+    if menu == 'p1team review':
+        p1teamreview()
+
+    if menu == 'p2pick1':
+        p2pick1()
+    if menu == 'p2pick2':
+        p2pick2()
+    if menu == 'p2pick3':
+        p2pick3()
+    if menu == 'p2pick4':
+        p2pick4()
+    if menu == 'p2pick5':
+        p2pick5()
+    if menu == 'team review':
+        teamReview()
+    if menu == 'Results':
+        results()
+    # for rect in rectList2:
+    #     if rect.collidepoint(mx, my) and mb[0] == 1:
+    #         screen.fill(bg)
+    #         screen.blit(myPicList[12], p3Select1)
+    #         screen.blit(myPicList[13], p3Select2)
+    #         screen.blit(myPicList[14], p3Select3)
+    #         screen.blit(myPicList[15], p3Select4)
+    #         screen.blit(myPicList[16], p3Select5)
+    #         screen.blit(myPicList[17], p3Select6)
 
     display.flip()
 
